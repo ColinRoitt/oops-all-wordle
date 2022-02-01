76ff8153c23de4382fd619222b39511ab505454a
Colin <roittcolin@gmail.com>
2022-02-01 13:37:34 +0000

    update CSS selector

--- src/components/KeyboardKey.js
+++ src/components/KeyboardKey.js
@@ -2,10 +2,8 @@ import { useSelector } from "react-redux";
 
 export default ({ letter, type }) => {
 	const round = useSelector((state) => state.round);
-	// const grid = useSelector((state) => state.grid);
 	const currentWord = useSelector((state) => state.currentWord);
 	const grid = useSelector((state) => state.grid).slice(0, round);
-	// console.log(grid);
 
 	const isPlayed = grid.reduce(
 		(acc, word) => acc || word.includes(letter),
@@ -22,16 +20,15 @@ export default ({ letter, type }) => {
 		false
 	);
 
-	// console.log(letter, isValid, isPlayed);
 	const color = (() => {
 		if (isValidAndCorrectPosition) {
-			return "key-correct";
+			return "correct";
 		}
 		if (isValid) {
-			return "key-in-word";
+			return "in-word";
 		}
 		if (isPlayed) {
-			return "key-used";
+			return "used";
 		}
 		return "";
 	})();
