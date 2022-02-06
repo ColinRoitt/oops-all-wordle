11bba0eed0367cdaace1fb5f785d1342b9dc17ec
Colin <roittcolin@gmail.com>
2022-02-06 21:43:58 +0000

    add light mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector, useStore } from "react-redux";
 import { words } from "./data/words";
 import { nonPlayableWords } from "./data/nonPlayableWords";
 import Game from "./components/Game";
 
 function App() {
 	const dispatch = useDispatch();
+
+	const [lightMode, setLightMode] = useState(
+		!!JSON.parse(localStorage.getItem("lightMode"))
+	);
+
+	const toggleLightMode = () => {
+		setLightMode(!lightMode);
+		localStorage.setItem("lightMode", !lightMode);
+	};
+
+	const intialiseLocalStorage = () => {
+		const currentSaveGames = localStorage.getItem("savedGames");
+		if (!currentSaveGames) {
+			localStorage.setItem("savedGames", JSON.stringify([]));
+		}
+	};
 
 	useEffect(() => {
 		dispatch({ type: "SET_WORDS", payload: words });
@@ -16,11 +32,16 @@ function App() {
 			// payload: "POISE",
 			payload: words[Math.floor(Math.random() * words.length)].toUpperCase(),
 		});
+		intialiseLocalStorage();
 	}, []);
 
 	return (
-		<div className="App">
-			<h1>Collection-of-wordsle</h1>
+		<div className={`App ${lightMode ? "light-mode" : ""}`}>
+			<div className="header">
+				<div className="spacer">C</div>
+				<h1>Collection-of-wordsle</h1>
+				<i onClick={toggleLightMode} className="fas fa-lightbulb"></i>
+			</div>
 			<Game />
 		</div>
 	);
