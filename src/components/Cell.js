0e81d381c3c741c92daac32678e8965863870770
Colin <roittcolin@gmail.com>
2022-02-11 19:39:14 +0000

    add flip animation to card for reveal

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,10 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
 
-export default ({ letter = "", color, reveal }) => {
+export default ({ letter = "", color, reveal, positionIndex }) => {
+	const transitionDelay = {
+		transitionDelay: `${positionIndex * 0.4}s`,
+	};
 	return (
-		<div className={`cell ${color} ${reveal ? "past" : ""}`}>
-			<div className={`cell-content`}>
-				<p>{letter}</p>
+		<div className="cell-scene">
+			<div
+				className={`cell-cont ${reveal ? "is-flipped" : ""}`}
+				style={transitionDelay}
+			>
+				<div className={`cell cell--front`}>
+					<div className={`cell-content`}>
+						<p>{letter}</p>
+					</div>
+				</div>
+				<div className={`cell cell--back past ${color}`}>
+					<div className={`cell-content`}>
+						<p>{letter}</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
